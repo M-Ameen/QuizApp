3fe1fd165d53b4bad9e161d519add58d9998fadd
M-Ameen <ameen.akbar2000@gmail.com>
2022-07-18 01:40:33 +0500

    Created another activity for QuizQuestions

---
+++ app/src/main/java/com/example/quizapp/Constants.kt
@@ -0,0 +1,13 @@
+package com.example.quizapp
+
+object Constants {
+//    fun getQuestions(): ArrayList<Questions> {
+//        val questionslist= ArrayList<Questions>()
+//
+//        val que1=Questions(
+//            1,"what country this flag belong to",R.drawable.
+//
+//        )
+//
+//    }
+}
--- app/src/main/java/com/example/quizapp/MainActivity.kt
+++ app/src/main/java/com/example/quizapp/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.example.quizapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.quizapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnStart.setOnClickListener {
+
+            if (binding.etName.text!!.isEmpty()){
+                Toast.makeText(this,"Please enter your name",Toast.LENGTH_SHORT).show()
+            }else{
+                val intent=Intent(this,QuizQuestionsActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+
+        }
     }
 }
